653cb23ef11566d56a55885660e18b8e7d38e991
Odin Dutton <odindutton@gmail.com>
2019-09-29 10:39:30 +1000

    Run VM loop in a thread to keep it separate from window updates

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use clap::{App, Arg};
 use minifb::{Scale, Window, WindowOptions};
 use std::fs::File;
 use std::io::Read;
-use std::thread::sleep;
+use std::sync::mpsc;
+use std::thread::{self, sleep};
 use std::time::{Duration, Instant};
 
 fn main() {
@@ -23,8 +24,30 @@ fn main() {
         .read_to_end(&mut rom)
         .expect("unable to read ROM");
 
-    let mut vm = Vm::new();
-    vm.load_rom(&rom);
+    let (tx_buf, rx_buf) = mpsc::channel::<Vec<u32>>();
+    let (tx_key, rx_key) = mpsc::channel::<Option<Key>>();
+
+    thread::spawn(move || {
+        let mut vm = Vm::new();
+        vm.load_rom(&rom);
+
+        let mut last_instant = Instant::now();
+        loop {
+            sleep(Duration::from_micros(1660) - last_instant.elapsed());
+
+            vm.key = rx_key.try_iter().last().unwrap_or(None);
+
+            let instruction = vm.fetch();
+            let opcode = opcode::decode(instruction);
+            vm.execute(opcode);
+
+            tx_buf
+                .send(vm.frame_buffer.buffer().clone())
+                .expect("unable to send buffer");
+
+            last_instant = Instant::now();
+        }
+    });
 
     let window_options = WindowOptions {
         scale: Scale::X8,
@@ -33,33 +56,18 @@ fn main() {
     let mut window =
         Window::new("CHIP-8", WIDTH, HEIGHT, window_options).expect("could open window");
 
-    let mut last_instant = Instant::now();
     while window.is_open() {
-        sleep(Duration::from_micros(1660) - last_instant.elapsed());
-
         window.get_keys().map(|keys| {
-            vm.key = None;
-
-            for k in keys {
-                let key = Key::from(k);
-                match key {
-                    Some(_) => {
-                        vm.key = key;
-                        break;
-                    }
-                    None => (),
-                }
-            }
+            tx_key
+                .send(keys.iter().find_map(|k| Key::from(*k)))
+                .expect("key send failed")
         });
 
-        let instruction = vm.fetch();
-        let opcode = opcode::decode(instruction);
-        vm.execute(opcode);
-
-        window
-            .update_with_buffer(&vm.frame_buffer.buffer())
-            .expect("could not update buffer");
-
-        last_instant = Instant::now();
+        match rx_buf.try_iter().last() {
+            Some(buffer) => window
+                .update_with_buffer(&buffer)
+                .expect("could not update buffer"),
+            None => window.update(),
+        }
     }
 }
